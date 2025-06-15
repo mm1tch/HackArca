e76ac347e7307666acba4056dfc668d430c3f22d
carolavazquez <149699651+carolavazquez@users.noreply.github.com>
2025-06-15 06:49:10 -0600

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -44,6 +44,35 @@ app.get("/api/sucursales", async (req, res) => {
   }
 });
 
+app.get("/api/visitas", async (req, res) => {
+  const url = `http://localhost:5050/api/visitas`;
+  console.log("🔁 Reenviando a Flask:", url);
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("❌ Error reenviando visitas:", error);
+    res.status(500).json({ error: "Error al obtener visitas" });
+  }
+});
+
+// 🔁 Reenviar surveys
+app.get("/api/surveys", async (req, res) => {
+  const url = `http://localhost:5050/api/surveys`;
+  console.log("🔁 Reenviando a Flask:", url);
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("❌ Error reenviando surveys:", error);
+    res.status(500).json({ error: "Error al obtener surveys" });
+  }
+});
+
 app.get("/api/users/:id", (req, res) => {
   res.json({
     id: req.params.id,
@@ -55,3 +84,4 @@ app.get("/api/users/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Backend corriendo en http://localhost:${PORT}`);
 });
+
